a69751d9f9263c47d75394ebe4812a6205a1873e
MatrixTan <hongsong.tan@wantgame.net>
2013-07-29 13:50:13 +0800

    -add hook function AlphaBlend & TransparentBlt

--- iBrowser/iBrowser/cross_process_render_helper.cpp
+++ iBrowser/iBrowser/cross_process_render_helper.cpp
@@ -13,6 +13,8 @@
 PFuncBitBlt CrossProcessRenderHelper::s_BitBlt = NULL;
 PFuncGetCursorPos CrossProcessRenderHelper::s_GetCursorPos = NULL;
 PFuncSetCursor CrossProcessRenderHelper::s_SetCursor = NULL;
+PFunAlphaBlend CrossProcessRenderHelper::s_AlphaBlend = NULL;
+PFunTransparentBlt CrossProcessRenderHelper::s_TransparentBlt = NULL;
 
 
 void CrossProcessRenderHelper::SetHost( HWND hHost )
@@ -35,6 +37,16 @@ BOOL CrossProcessRenderHelper::CustomBitBlt( HDC hdc, int x, int y, int cx, int 
 
 		HWND hTarget = ::WindowFromDC(hdc);
 		if ( hTarget != m_hCore){
+			//render the core and child window only.
+			HWND hParent = ::GetParent(hTarget);
+			while (hParent != m_hCore && (hParent = ::GetParent(hParent))){
+				
+			}
+			if (hParent == NULL){
+				return FALSE;
+			}
+
+			//add the offset of child window rect.
 			RECT rectTarget = {0};
 			::GetWindowRect(hTarget, &rectTarget);
 			RECT rectCore = {0};
@@ -67,6 +79,11 @@ BOOL CrossProcessRenderHelper::CustomBitBlt( HDC hdc, int x, int y, int cx, int 
 	return FALSE;
 }
 
+Gdiplus::ARGB s_colors[]
+={
+	0x2CFF0000, 0x2C00FF00, 0x2C0000FF, 0x2CFFFF00, 0x2C00FFFF
+};
+
 void CrossProcessRenderHelper::RenderOnHost( HWND hHost, void* pScreenData )
 {
 	if (GlobalSingleton::GetInstance()->GetCrossProcessRender()){
@@ -84,31 +101,19 @@ void CrossProcessRenderHelper::RenderOnHost( HWND hHost, void* pScreenData )
 		::SetBitmapBits(hBitmap, sizeof(DWORD)*cx*cy, (void*)(pData+4));
 		Gdiplus::Graphics g(h);
 		Gdiplus::Bitmap bitmap(hBitmap, NULL);
-
-		///////////////TEST////////////////
-		//for (int w = 0; w < bitmap.GetWidth(); ++w){
-		//	for (int h = 0; h < bitmap.GetHeight(); ++h){
-		//		Gdiplus::Color color;
-		//		bitmap.GetPixel(w,h,&color);
-		//		static int s=0;
-		//		s = (s+1)%3;
-		//		if (s == 0)
-		//		{
-		//			Gdiplus::Color newColor(255, 255, color.GetG(), color.GetB());
-		//			bitmap.SetPixel(w,h, newColor);
-		//		}else if (s == 1)
-		//		{
-		//			Gdiplus::Color newColor(255, color.GetR(), 255, color.GetB());
-		//			bitmap.SetPixel(w,h, newColor);
-		//		}else if (s == 2)
-		//		{
-		//			Gdiplus::Color newColor(255, color.GetR(), color.GetG(), 255);
-		//			bitmap.SetPixel(w,h, newColor);
-		//		}				
-		//	}
-		//}
-		///////////////////////////////
+		
 		g.DrawImage(&bitmap, (INT)x, (INT)y, (INT)cx, (INT)cy);
+
+		///////////TEST/////////////////////////
+		static int colerIndex = 0;
+		if (colerIndex > 4)
+		{
+			colerIndex = 0;
+		}
+		Gdiplus::SolidBrush brush(s_colors[colerIndex++]);
+		g.FillRectangle(&brush,(INT)x, (INT)y, (INT)cx, (INT)cy);
+		/////////////////////////////////////////////
+
 		::DeleteObject(hBitmap);
 		::ReleaseDC(hHost, h);
 	}
@@ -130,6 +135,8 @@ bool CrossProcessRenderHelper::StartHooksInCoreProcess( void )
 		HookLibAndProc("gdi32.dll", "BitBlt", (void*)HOOK_BitBlt, (void**)&s_BitBlt);
 		HookLibAndProc("user32.dll", "GetCursorPos", (void*)HOOK_GetCursorPos, (void**)&s_GetCursorPos);
 		HookLibAndProc("user32.dll", "SetCursor", (void*)HOOK_SetCursor, (void**)&s_SetCursor);
+		HookLibAndProc("msimg32.dll", "AlphaBlend", (void*)HOOK_AlphaBlend, (void**)&s_AlphaBlend);
+		HookLibAndProc("msimg32.dll", "TransparentBlt", (void*)HOOK_TransparentBlt, (void**)&s_TransparentBlt);
 	}
 	return true;
 }
@@ -154,5 +161,57 @@ HCURSOR WINAPI CrossProcessRenderHelper::HOOK_SetCursor( HCURSOR hCursor )
 {
 	return s_SetCursor(hCursor);
 }
+
+BOOL WINAPI CrossProcessRenderHelper::HOOK_AlphaBlend( HDC hdcDest
+	, int xoriginDest
+	, int yoriginDest
+	, int wDest
+	, int hDest
+	, HDC hdcSrc
+	, int xoriginSrc
+	, int yoriginSrc
+	, int wSrc
+	, int hSrc
+	, BLENDFUNCTION ftn )
+{
+	return s_AlphaBlend(hdcDest
+		,xoriginDest
+		,yoriginDest
+		,wDest
+		,hDest
+		,hdcSrc
+		,xoriginSrc
+		,yoriginSrc
+		,wSrc
+		,hSrc
+		,ftn);
+}
+
+BOOL WINAPI CrossProcessRenderHelper::HOOK_TransparentBlt( HDC hdcDest,
+	int xoriginDest,
+	int yoriginDest,
+	int wDest,
+	int hDest,
+	HDC hdcSrc,
+	int xoriginSrc,
+	int yoriginSrc,
+	int wSrc,
+	int hSrc,
+	UINT crTransparent)
+{
+	return s_TransparentBlt(hdcDest,
+		xoriginDest,
+		yoriginDest,
+		wDest,
+		hDest,
+		hdcSrc,
+		xoriginSrc,
+		yoriginSrc,
+		wSrc,
+		hSrc,
+		crTransparent);
+}
+
+
 
 
--- iBrowser/iBrowser/cross_process_render_helper.h
+++ iBrowser/iBrowser/cross_process_render_helper.h
@@ -7,10 +7,36 @@
 
 #include "Base/t_thread_singleton.h"
 #include "api_hook.h"
+#include <WinGDI.h>
 
 typedef BOOL (WINAPI *PFuncBitBlt)(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop);
 typedef BOOL (WINAPI *PFuncGetCursorPos)(LPPOINT lpPoint);
 typedef HCURSOR (WINAPI *PFuncSetCursor)(HCURSOR hCursor);
+typedef BOOL (WINAPI *PFunAlphaBlend)(
+	HDC hdcDest,
+	int xoriginDest,
+	int yoriginDest,
+	int wDest,
+	int hDest,
+	HDC hdcSrc,
+	int xoriginSrc,
+	int yoriginSrc,
+	int wSrc,
+	int hSrc,
+	BLENDFUNCTION ftn);
+typedef BOOL (WINAPI *PFunTransparentBlt)(
+	HDC hdcDest,
+	int xoriginDest,
+	int yoriginDest,
+	int wDest,
+	int hDest,
+	HDC hdcSrc,
+	int xoriginSrc,
+	int yoriginSrc,
+	int wSrc,
+	int hSrc,
+	UINT crTransparent);
+
 
 class CrossProcessRenderHelper 
 	: public TThreadSingleton<CrossProcessRenderHelper, ETTS_CrossProcessRenderHelper>
@@ -24,6 +50,28 @@ public:
 	static BOOL WINAPI HOOK_BitBlt( HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop );
 	static BOOL WINAPI HOOK_GetCursorPos(LPPOINT lpPoint);
 	static HCURSOR WINAPI HOOK_SetCursor(HCURSOR hCursor);
+	static BOOL WINAPI HOOK_AlphaBlend(HDC hdcDest,
+		int xoriginDest,
+		int yoriginDest,
+		int wDest,
+		int hDest,
+		HDC hdcSrc,
+		int xoriginSrc,
+		int yoriginSrc,
+		int wSrc,
+		int hSrc,
+		BLENDFUNCTION ftn);
+	static BOOL WINAPI HOOK_TransparentBlt(HDC hdcDest,
+		int xoriginDest,
+		int yoriginDest,
+		int wDest,
+		int hDest,
+		HDC hdcSrc,
+		int xoriginSrc,
+		int yoriginSrc,
+		int wSrc,
+		int hSrc,
+		UINT crTransparent);
 
 	///Main Process Method
 	static void RenderOnHost(HWND hHost, void* pScreenData);
@@ -31,10 +79,11 @@ protected:
 private:
 	HWND m_hHost;
 	HWND m_hCore;
-
 	static PFuncBitBlt s_BitBlt;
 	static PFuncGetCursorPos s_GetCursorPos;
 	static PFuncSetCursor s_SetCursor;
+	static PFunAlphaBlend s_AlphaBlend;
+	static PFunTransparentBlt s_TransparentBlt;
 };
 
 
